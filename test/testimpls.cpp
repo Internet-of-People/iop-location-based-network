8d255c315fbcbbf445b36097c3fcc5036ae90d38
izolyomi <istvan.zolyomi.ext@nokia.com>
2016-10-21 20:49:32 +0200

    Added missing files from last commit

---
+++ test/testimpls.cpp
@@ -0,0 +1,47 @@
+#include "testimpls.hpp"
+
+using namespace std;
+
+
+
+double DummySpatialDatabase::GetDistance(const GpsLocation& one, const GpsLocation& other) const
+{
+    // TODO
+    return 0;
+}
+
+
+// void DummySpatialDatabase::Store(const NodeLocation& node)
+// {
+//     // TODO
+// }
+// 
+// 
+// NodeLocation DummySpatialDatabase::Load(const string& nodeId) const
+// {
+//     // TODO
+//     return NodeLocation();
+// }
+// 
+// 
+// void DummySpatialDatabase::Remove(const string& nodeId)
+// {
+//     // TODO
+// }
+// 
+// 
+// vector<NodeLocation> DummySpatialDatabase::GetNeighbourHood() const
+// {
+//     // TODO
+//     return vector<NodeLocation>();
+// }
+// 
+// 
+// vector<NodeLocation> DummySpatialDatabase::GetNodesCloseTo(
+//     const GpsLocation& position, double radius, size_t maxNodeCount) const
+// {
+//     // TODO
+//     return vector<NodeLocation>();
+// }
+
+
---
+++ test/testimpls.hpp
@@ -0,0 +1,27 @@
+#ifndef __IMPLEMENTATIONS_FOR_TESTING_H__
+#define __IMPLEMENTATIONS_FOR_TESTING_H__
+
+#include <string>
+
+#include "geonet.hpp"
+
+
+
+class DummySpatialDatabase : public SpatialDatabase
+{
+public:
+    
+    double GetDistance(const GpsLocation &one, const GpsLocation &other) const override;
+    
+//     std::vector<NodeLocation> GetNeighbourHood() const override;
+//     std::vector<NodeLocation> GetNodesCloseTo(
+//         const GpsLocation &position, double radius, size_t maxNodeCount) const override;
+//         
+//     void Store(const NodeLocation &node) override;
+//     NodeLocation Load(const std::string &nodeId) const override;
+//     void Remove(const std::string &nodeId) override;
+};
+
+
+
+#endif // __IMPLEMENTATIONS_FOR_TESTING_H__
