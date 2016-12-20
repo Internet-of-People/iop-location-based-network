463a044729eac2b86880503b9ceeeb0f0ad577c2
izolyomi <istvan.zolyomi.ext@nokia.com>
2016-12-20 16:53:51 +0100

    Fixed typo

--- package/libprotobuf3/pack-common.sh
+++ package/libprotobuf3/pack-common.sh
@@ -7,7 +7,7 @@ sudo checkinstall \
     --pkgsource="https://github.com/google/protobuf.git" \
     --pkglicense=BSD \
     --pkggroup=net \
-    --pkgname=iibprotobuf10 \
+    --pkgname=libprotobuf10 \
     --pkgversion=3.0.0-b1 \
     --pkgarch=$(dpkg --print-architecture) \
     $@
