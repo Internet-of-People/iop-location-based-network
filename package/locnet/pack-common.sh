3c37fc5c933a222c792e6bdec9947d061f981a8f
izolyomi <istvan.zolyomi.ext@nokia.com>
2017-02-06 16:07:43 +0100

    Initial experimental release 0.1.0

--- package/locnet/pack-common.sh
+++ package/locnet/pack-common.sh
@@ -9,6 +9,6 @@ sudo checkinstall \
     --pkglicense=MIT \
     --pkggroup=net \
     --pkgname=iop-locnet \
-    --pkgversion=0.0.1-a7 \
+    --pkgversion=0.1.0 \
     --pkgarch=$(dpkg --print-architecture) \
     $@
