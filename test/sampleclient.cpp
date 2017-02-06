1d83fe5b913f58c6126e0d0081bc10ddfc438204
izolyomi <istvan.zolyomi.ext@nokia.com>
2017-02-06 16:04:27 +0100

    Use operations only from a single (client) interface because ports of interfaces are now separated

--- test/sampleclient.cpp
+++ test/sampleclient.cpp
@@ -51,69 +51,70 @@ int main(int argc, const char* const argv[])
         LOG(INFO) << "Got " << randomNodes.size() << " random nodes";
         for (const auto &node : randomNodes)
             { LOG(INFO) << "  " << node; }
-        
-        thread msgThread( [session, dispatcher]
-        {
-            try
-            {
-                LOG(INFO) << "Sending getneighbournodes request";
-                iop::locnet::Request request;
-                request.mutable_localservice()->mutable_getneighbournodes()->set_keepaliveandsendupdates(true);
-                unique_ptr<iop::locnet::Response> response = dispatcher->Dispatch(request);
-                
-                LOG(INFO) << "Got " << response->localservice().getneighbournodes().nodes_size()
-                        << " neighbours in the reponse";
-                for (int idx = 0; idx < response->localservice().getneighbournodes().nodes_size(); ++idx)
-                {
-                    const iop::locnet::NodeInfo &neighbour =
-                        response->localservice().getneighbournodes().nodes(idx);
-                    string buffer;
-                    google::protobuf::TextFormat::PrintToString(neighbour, &buffer);
-                    LOG(INFO) << "  Neighbour " << buffer;
-                }
-                
-                while (! ShutdownRequested)
-                {
-                    LOG(INFO) << "Reading change notification";
-                    unique_ptr<iop::locnet::MessageWithHeader> changeNote( session->ReceiveMessage() );
-                    if (! changeNote)
-                    {
-                        LOG(ERROR) << "Received empty message";
-                        break;
-                    }
-                    if ( ! changeNote->has_body() || ! changeNote->body().has_request() ||
-                         ! changeNote->body().request().has_localservice() )
-                    {
-                        LOG(ERROR) << "Received unexpected message";
-                        break;
-                    }
-                    
-//                     for (int idx = 0; idx < changeNote->body().request().localservice().neighbourhoodchanged().changes_size(); ++idx)
+
+//         // Test listening for neighbourhood notifications
+//         thread msgThread( [session, dispatcher]
+//         {
+//             try
+//             {
+//                 LOG(INFO) << "Sending getneighbournodes request";
+//                 iop::locnet::Request request;
+//                 request.mutable_localservice()->mutable_getneighbournodes()->set_keepaliveandsendupdates(true);
+//                 unique_ptr<iop::locnet::Response> response = dispatcher->Dispatch(request);
+//                 
+//                 LOG(INFO) << "Got " << response->localservice().getneighbournodes().nodes_size()
+//                         << " neighbours in the reponse";
+//                 for (int idx = 0; idx < response->localservice().getneighbournodes().nodes_size(); ++idx)
+//                 {
+//                     const iop::locnet::NodeInfo &neighbour =
+//                         response->localservice().getneighbournodes().nodes(idx);
+//                     string buffer;
+//                     google::protobuf::TextFormat::PrintToString(neighbour, &buffer);
+//                     LOG(INFO) << "  Neighbour " << buffer;
+//                 }
+//                 
+//                 while (! ShutdownRequested)
+//                 {
+//                     LOG(INFO) << "Reading change notification";
+//                     unique_ptr<iop::locnet::MessageWithHeader> changeNote( session->ReceiveMessage() );
+//                     if (! changeNote)
 //                     {
-//                         const iop::locnet::NeighbourhoodChange &change =
-//                             changeNote->body().request().localservice().neighbourhoodchanged().changes(idx);
-//                             
-//                         string buffer;
-//                         google::protobuf::TextFormat::PrintToString(change, &buffer);
-//                         LOG(INFO) << "  Change: " << buffer;
+//                         LOG(ERROR) << "Received empty message";
+//                         break;
 //                     }
-                    
-                    LOG(INFO) << "Sending acknowledgement";
-                    iop::locnet::MessageWithHeader changeAckn;
-                    changeAckn.mutable_body()->mutable_response()->mutable_localservice()->mutable_neighbourhoodupdated();
-                    session->SendMessage(changeAckn);
-                }
-            }
-            catch (exception &ex)
-            {
-                LOG(ERROR) << "Error: " << ex.what();
-            }
-            LOG(INFO) << "Stopped reading notifications";
-        } );
-        msgThread.detach();
-        
-        while (! ShutdownRequested)
-            { this_thread::sleep_for( chrono::milliseconds(50) ); }
+//                     if ( ! changeNote->has_body() || ! changeNote->body().has_request() ||
+//                          ! changeNote->body().request().has_localservice() )
+//                     {
+//                         LOG(ERROR) << "Received unexpected message";
+//                         break;
+//                     }
+//                     
+// //                     for (int idx = 0; idx < changeNote->body().request().localservice().neighbourhoodchanged().changes_size(); ++idx)
+// //                     {
+// //                         const iop::locnet::NeighbourhoodChange &change =
+// //                             changeNote->body().request().localservice().neighbourhoodchanged().changes(idx);
+// //                             
+// //                         string buffer;
+// //                         google::protobuf::TextFormat::PrintToString(change, &buffer);
+// //                         LOG(INFO) << "  Change: " << buffer;
+// //                     }
+//                     
+//                     LOG(INFO) << "Sending acknowledgement";
+//                     iop::locnet::MessageWithHeader changeAckn;
+//                     changeAckn.mutable_body()->mutable_response()->mutable_localservice()->mutable_neighbourhoodupdated();
+//                     session->SendMessage(changeAckn);
+//                 }
+//             }
+//             catch (exception &ex)
+//             {
+//                 LOG(ERROR) << "Error: " << ex.what();
+//             }
+//             LOG(INFO) << "Stopped reading notifications";
+//         } );
+//         msgThread.detach();
+//         
+//         while (! ShutdownRequested)
+//             { this_thread::sleep_for( chrono::milliseconds(50) ); }
         
         return 0;
     }
