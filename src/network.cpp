5a7afdd662e52ca0af19a49d46e431c962db3c62
izolyomi <istvan.zolyomi.ext@nokia.com>
2017-02-22 09:49:10 +0100

    Log notification error details

--- src/network.cpp
+++ src/network.cpp
@@ -575,7 +575,7 @@ void ProtoBufTcpStreamChangeListener::AddedNode(const NodeDbEntry& node)
         }
         catch (exception &ex)
         {
-            LOG(ERROR) << "Failed to send change notification";
+            LOG(ERROR) << "Failed to send change notification: " << ex.what();
             Deregister();
         }
     }
@@ -599,7 +599,7 @@ void ProtoBufTcpStreamChangeListener::UpdatedNode(const NodeDbEntry& node)
         }
         catch (exception &ex)
         {
-            LOG(ERROR) << "Failed to send change notification";
+            LOG(ERROR) << "Failed to send change notification: " << ex.what();
             Deregister();
         }
     }
@@ -622,7 +622,7 @@ void ProtoBufTcpStreamChangeListener::RemovedNode(const NodeDbEntry& node)
         }
         catch (exception &ex)
         {
-            LOG(ERROR) << "Failed to send change notification";
+            LOG(ERROR) << "Failed to send change notification: " << ex.what();
             Deregister();
         }
     }
