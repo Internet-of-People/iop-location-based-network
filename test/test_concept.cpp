909912fad01dd538de5f08987f73e17c3268e77f
István Zólyomi <istvan.zolyomi@gmail.com>
2017-09-13 09:49:42 +0200

    Added new test for global network behaviour

---
+++ test/test_concept.cpp
@@ -0,0 +1,19 @@
+#include <catch.hpp>
+#include <easylogging++.h>
+
+#include "testdata.hpp"
+#include "testimpls.hpp"
+
+using namespace std;
+using namespace LocNet;
+
+
+
+SCENARIO("Conceptual correctness of the algorithm in the global network", "[concept]")
+{
+    GIVEN("A network") {
+        THEN("It works fine") {
+            REQUIRE(false);
+        }
+    }
+}
