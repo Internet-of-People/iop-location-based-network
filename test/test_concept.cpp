eefda807947dd03a831bace56de2d79d78e0d810
István Zólyomi <istvan.zolyomi@gmail.com>
2017-09-13 17:32:46 +0200

    Correctly parse CSV file

--- test/test_concept.cpp
+++ test/test_concept.cpp
@@ -22,20 +22,51 @@ const char PATH_SEPARATOR =
 
 SCENARIO("Conceptual correctness of the algorithm organizing the global network", "[concept]")
 {
-    GIVEN("A network")
+    GIVEN("A map of the biggest cities")
     {
         // CSV input is in the same file as the test executable
         const string &execPath = Config::Instance().execPath();
         int pos = execPath.rfind(PATH_SEPARATOR);
-        string execDir( execPath.substr(0, pos+1) );
+        string execDir( execPath.substr(0, pos+1) ); // if not found, pos will be -1, substr will be empty
         
         ifstream citiesCsvFile( execDir + PATH_SEPARATOR + "worldcities.csv");
         REQUIRE( citiesCsvFile.is_open() );
 
-        string line;
-        while( getline(citiesCsvFile, line) )
+        // Skip first (header) line
+        string csvLine;
+        REQUIRE( getline(citiesCsvFile, csvLine) );
+        
+        // Process entries from CSV file
+        while( getline(citiesCsvFile, csvLine) )
         {
-            // TODO
+            stringstream lineStream(csvLine);
+            vector<string> tokens;
+            
+            string token;
+            while ( lineStream.good() )
+            {
+                if (lineStream.peek() != '"')
+                {
+                    // Read value until next comma
+                    if ( getline(lineStream, token, ',') )
+                        { tokens.push_back(token); }
+                }
+                else
+                {
+                    // Respect commas inside quotes
+                    getline(lineStream, token, '"');
+                    if ( getline(lineStream, token, '"') )
+                    {
+                        tokens.push_back(token);
+                        getline(lineStream, token, ','); // consume comma after quotes
+                    }
+                }
+            }
+            
+            for (auto token : tokens)
+                { cout << token << "\t"; }
+            cout << endl;
+            REQUIRE( tokens.size() == 9 );
         }
         
         THEN("It works fine") {
